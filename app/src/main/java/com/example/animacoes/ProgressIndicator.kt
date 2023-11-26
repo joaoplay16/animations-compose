fcc9c1d442dcc3e1e33f2a78475c650c158745b7
João Pedro <joaoplay16@gmail.com>
2023-11-25 21:37:48 -0300

    Change package of ProgressIndicator

--- app/src/main/java/com/example/animacoes/ProgressIndicator.kt
+++ app/src/main/java/com/example/animacoes/ProgressIndicator.kt
@@ -1,4 +1,4 @@
-package com.example.testcompose.ui.animation
+package com.example.animacoes
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
